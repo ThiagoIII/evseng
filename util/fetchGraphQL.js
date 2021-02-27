4ccd1804d9e198cf09f5f5894a9c214bcb5dee70
ThiagoIII <thiagoiii@hotmail.com>
2021-02-27 13:13:50 -0300

    adding next_public to env vars........

--- util/fetchGraphQL.js
+++ util/fetchGraphQL.js
@@ -15,30 +15,31 @@ const query = `query
 
 export async function fetchGraphQLDev() {
     return fetch(
-        `https://graphql.contentful.com/content/v1/spaces/${process.env.contentful_space_id}`,
+        `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}`,
         {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: `Bearer ${process.env.contentful_access_token}`
+                Authorization: `Bearer lHPRSH-89A-HNrFKRsQGNj2KWIIRMD4hbeC5E1Hn858`
             },
             body: JSON.stringify({ query })
         }
     ).then(response => response.json())
 }
-export async function fetchGraphQLProd() {
+
+/* export async function fetchGraphQLProd() {
     return fetch(
-        `https://graphql.contentful.com/content/v1/spaces/${process.env.contentful_space_id}`,
+        `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
         {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: `Bearer ${process.env.contentful_access_token}`
+                Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`
             },
             body: JSON.stringify({ query })
         }
     ).then(response => response.json())
-}
+} */
 
 /* function extractPostEntries(fetchResponse) {
   return fetchResponse?.data?.cardCollection?.items
