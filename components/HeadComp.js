5fcd116f7c3e0ce36fd89cddcdc08cbc60705e59
ThiagoIII <thiagoiii@hotmail.com>
2021-02-28 18:28:27 -0300

    fixed link

--- components/HeadComp.js
+++ components/HeadComp.js
@@ -13,10 +13,38 @@ const HeadComp = () => (
             href="https://s.pageclip.co/v1/pageclip.css"
             media="screen"
         ></link>
+        <link rel="me" href="https://github.com/ThiagoIII" type="text/html" />
+        <meta property="fb:app_id" content="123456789" />
+        <meta property="og:url" content="https://example.com/page.html" />
+        <meta property="og:type" content="website" />
+        <meta
+            property="og:title"
+            content="EVS Engenharia - Qualidade e confiança"
+        />
+        <meta property="og:image" content="/facebook.svg" />
+        <meta
+            property="og:image:alt"
+            content="Logo da EVS Engenharia, dois telhados azuis se sobrepondo parcialmente"
+        />
+        <meta
+            property="og:description"
+            content="Empresa EVS Engenharia - Qualidade e confiança. Sede em Umuarama no Paraná"
+        />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="article:author" content="Thiago Terceiro" />
         <meta
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <meta
+            name="description"
+            content="Site da empresa de engenharia EVS Engenharia com sede em Umuarama - Paraná"
+        />
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
+        <meta name="subject" content="Engenharia" />
+        <meta name="geo.region" content="BR-PR"></meta>
+        <meta name="geo.placename" content="Umuarama"></meta>
         <title>EVS Engenharia - Qualidade e Confiança</title>
         <link rel="icon" href="/favicon.ico" />
     </Head>
